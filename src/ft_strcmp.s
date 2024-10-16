28b687a3b5dffaaeff878db2f7044e2737a200ad
42-Ceasure <cglavieu@student.42.fr>
2024-10-16 16:53:54 +0200

    d

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -7,6 +7,7 @@ global _ft_strcmp
 section .text
 
     _ft_strcmp:
+        xor r8, r8
         mov rax, rdi
         cmp rax, 0
         je  exit_ft
@@ -15,17 +16,22 @@ section .text
     
 
     char_compare:
-        mov al, [rdi]
-        cmp al, [rsi]
+        mov r8b, [rdi]
+        cmp r8b, [rsi]
         jne str_not_equal
-        cmp al, 0
+        cmp r8b, 0
         je  exit_ft
         inc rdi
         inc rsi
         jmp char_compare
     
     str_not_equal:
-        sub rax, [rsi]
+        xor r8, r8
+        xor r9, r9
+        mov r9b, [rsi]
+        sub r8b, r9b
     
     exit_ft:
+        xor rax, rax
+        mov rax, r8b
         ret
