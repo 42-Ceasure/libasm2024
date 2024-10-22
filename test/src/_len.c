7b4a5155b4cf8c2a538322b4505c4a1a0c492431
42-Ceasure <cglavieu@student.42.fr>
2024-10-22 20:17:52 +0200

    itoa to finish

--- test/src/_len.c
+++ test/src/_len.c
@@ -1,6 +1,36 @@
 
 #include <lasm_test.h>
 
+
+
+
+
+char	*itoa(int nb)
+{
+	char	number[12];
+	int		neg;
+	long	mem;
+	int		i;
+
+	neg = 0;
+	mem = nb;
+	if (nb < 0)
+	{
+		mem = -nb;
+		neg = -1;
+	}
+	i = -neg;
+	while (++i && mem / 10 > 1)
+		mem = mem / 10;
+	printf("nb:%d\ni:%d\n", nb, i);
+	while (nb > 1 && i--)
+	{
+		number[i] = (nb % 10) + '0';
+		nb = nb / 10;
+	}
+	return ("ok");
+}
+
 void	test_strlen(int ac, char **av)
 {
 	int 	i;
@@ -11,6 +41,7 @@ void	test_strlen(int ac, char **av)
 	ft_write(1, "testing ft_strlen\n", 18);
 	while (i < ac)
 	{
+		itoa(atoi(av[i]));
 		len = ft_strlen(av[i]);
 		printf("av:%d\tlen = %d\n", i, len);
 		i++;
